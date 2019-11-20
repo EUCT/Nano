263d1b131fe5b01b773bde50c4d00182ac0751fd
EUCT <57327818+EUCT@users.noreply.github.com>
2019-11-20 13:13:22 +0700

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+# Variables
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red="\e[91m"
+green="\033[0;32m"
+light="\e[92m"
+
+
+###################################################
+#  CTRL + C
+###################################################
+
+echo -e " =============================================== "
+              " Author : Mr CR45H $red                                                                " | lolcat
+              " Feat     : Mr CR45H $white                                                            " | lolcat
+              " Contact  : 085755365813 $white                                                 " | lolcat
+              " =============================================== " 
+echo ""
+echo -e "	1.  install bahan" | lolcat
+echo -e "===================================" | lolcat
+echo -e "	2.  Hack Facebook " | lolcat
+echo -e "====================================" | lolcat
+echo -e "	3. install metasploit" | lolcat
+echo -e "====================================" | lolcat
+echo -e "	4. Spam Telepon Tokopedia" | lolcat
+
